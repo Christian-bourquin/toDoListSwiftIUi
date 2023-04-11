adcc8a1f4ea0f6a572f451c41e10e47fa96ee99e
christian bourquin <christian.bourquin23@d155.org>
2023-04-11 17:59:12 -0500

    Initial Commit

---
+++ toDoListSwiftIUi/toDoListSwiftIUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListSwiftIUiApp.swift
+//  toDoListSwiftIUi
+//
+//  Created by Christian Bourquin on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct toDoListSwiftIUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
